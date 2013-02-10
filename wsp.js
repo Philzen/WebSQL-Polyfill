90c8182db4f5bdddc61daf8ce91a4d194f92802e
Phil. Austermann <phil@patient-radio.de>
2013-02-10 02:44:34 +0100

    + Built db.transaction()-shim

--- wsp.js
+++ wsp.js
@@ -1,14 +1,31 @@
-	
 
 	if (typeof window.openDatabase === 'undefined') (function() {
 		
+		var Transaction = function () {
+			this.executeSql = function(sql, bindVars, successCallback, errorCallback) {
+				console.log('about to execute command: ' + sql);
+			};
+		};
+		
 		var Database = function Database (name, version, description, size) {
-			
+			var db = SQL.open();
+			this.transaction = function(transactionFunction, errorCallback, successCallback) {
+				if (arguments.length < 1) 
+					throw new TypeError('Not enough arguments');
+				if (typeof transactionFunction !== 'function' || (errorCallback !== undefined && typeof errorCallback !== 'function') || (successCallback !== undefined && typeof successCallback !== 'function')) 
+					throw new TypeError('At least one supplied argument is not a function');
+				
+				transactionFunction(new Transaction());
+			};
 		};
 		
 		function openDatabase(name, version, description, size) {
+			if (arguments.length < 4) {
+				throw new TypeError('Not enough arguments');
+			}
+			
 			return new Database(name, version, description, size);
-		}
+		};
 		
 
 		window.openDatabase = openDatabase;
